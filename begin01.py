316cb4759eb71815cd985dda4ef849e471d7edc9
nora4977 <nora4977@nodirayoqubjnova9@gmail.com>
2022-11-22 07:56:26 +0500

    01 done

--- begin01.py
+++ begin01.py
@@ -1,6 +1,7 @@
 # num_one o'zgaruvchiga 5 ni taminlang;
-num_one =5
+num_one = 5
 # num_two o'zgaruvchiga 4 ni taminlang;
-num_two =4
+num_two = 4
 # Yig'indisini chop eting;
-print("num_one + num_two")
+
+print (num_one+num_two)
